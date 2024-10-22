4a924552dd8366e545cde38a2848930a3acc2ce4
Felipe Lima <pegasusyt1@gmail.com>
2024-10-22 16:29:59 -0300

    ajustes no propertysearch

--- website/src/components_i/ui/PropertySearch.tsx
+++ website/src/components_i/ui/PropertySearch.tsx
@@ -45,9 +45,9 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
   return (
     <div className="flex flex-col items-center md:items-start relative z-10">
       {/* Versão Desktop */}
-      <div className="hidden md:flex bg-white p-4 rounded-lg shadow-md w-full md:w-[980px] justify-between items-center mt-4">
+      <div className="hidden md:flex bg-white p-4 rounded-lg shadow-md w-full md:w-[1100px] justify-between items-center mt-4">
         {/* Bloco de Localização */}
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-4">
           <div>
             <p className="text-gray-500 text-[14px]">Localização</p>
             <input
@@ -61,10 +61,10 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
         </div>
 
         {/* Divisor */}
-        <div className="border-l border-gray-300 h-10"></div>
+        <div className="border-l border-gray-300 h-10 mx-4"></div>
 
         {/* Bloco de Data de Entrada */}
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-4">
           <div>
             <p className="text-gray-500 text-[14px] ml-1">Data de Entrada</p>
             <input
@@ -77,10 +77,10 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
         </div>
 
         {/* Divisor */}
-        <div className="border-l border-gray-300 h-10"></div>
+        <div className="border-l border-gray-300 h-10 mx-4"></div>
 
         {/* Bloco de Data de Saída */}
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-4">
           <div>
             <p className="text-gray-500 text-[14px] ml-1">Data de Saída</p>
             <input
@@ -93,17 +93,17 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
         </div>
 
         {/* Divisor */}
-        <div className="border-l border-gray-300 h-10"></div>
+        <div className="border-l border-gray-300 h-10 mx-4"></div>
 
         {/* Bloco de Preço */}
-        <div className="flex items-center space-x-2 relative">
+        <div className="flex items-center space-x-4 relative">
           <div>
             <p className="text-gray-500 text-[14px] ml-1">Preço</p>
-            <div className="relative">
+            <div className="relative w-[160px]">
               <select
                 value={faixaPreco}
                 onChange={(e) => setFaixaPreco(e.target.value)}
-                className="font-bold text-[16px] border-none outline-none bg-white appearance-none p-1"
+                className="font-bold text-[16px] border-none outline-none bg-white appearance-none p-2 w-full ml-[-10px]"
               >
                 <option value="R$0–R$2,500">R$0–R$2,500</option>
                 <option value="R$2,500–R$5,000">R$2,500–R$5,000</option>
@@ -116,19 +116,21 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
         </div>
 
         {/* Divisor */}
-        <div className="border-l border-gray-300 h-10"></div>
+        <div className="border-l border-gray-300 h-10 mx-4"></div>
 
         {/* Bloco de Tipo de Propriedade */}
-        <div className="flex items-center space-x-2 relative">
+        <div className="flex items-center space-x-4 relative">
           <div>
-            <p className="text-gray-500 text-[14px] ml-1">Tipo de Propriedade</p>
-            <div className="relative">
+            <p className="text-gray-500 text-[14px] ml-1">
+              Tipo de Propriedade
+            </p>
+            <div className="relative w-[160px]">
               <select
                 value={tipoPropriedade}
                 onChange={(e) => setTipoPropriedade(e.target.value)}
-                className="font-bold text-[16px] border-none outline-none bg-white appearance-none p-1"
+                className="font-bold text-[16px] border-none outline-none bg-white appearance-none p-2 w-full"
               >
-                <option value="">Todos</option>{" "}
+                <option value="">Todos</option>
                 {/* Opção vazia para remover o filtro */}
                 {categorias.map((categoria) => (
                   <option key={categoria} value={categoria}>
@@ -143,13 +145,15 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
         </div>
 
         {/* Botão de Busca */}
-        <Button variant="large" size="large" onClick={handleSearch}>
-          Buscar
-        </Button>
+        <div>
+          <Button variant="large" size="large" onClick={handleSearch}>
+            Buscar
+          </Button>
+        </div>
       </div>
 
       {/* Versão Mobile */}
-      <div className="flex md:hidden flex-col items-start bg-white p-4 rounded-lg shadow-md w-full md:w-[980px] mx-12 mt-4">
+      <div className="flex md:hidden flex-col items-start bg-white p-4 rounded-lg shadow-md w-full mx-12 mt-4">
         <label className="mb-2 text-gray-500 text-[14px]">Localização</label>
         <input
           type="text"
@@ -158,7 +162,9 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
           onChange={(e) => setLocalizacao(e.target.value)}
           className="mb-4 p-6 border rounded-md w-full"
         />
-        <label className="mb-2 text-gray-500 text-[14px]">Data de Entrada</label>
+        <label className="mb-2 text-gray-500 text-[14px]">
+          Data de Entrada
+        </label>
         <input
           type="date"
           placeholder="Data de Entrada"
@@ -177,31 +183,33 @@ const PropertySearch = ({ onSearch }: { onSearch: (filters: any) => void }) => {
         <label className="mb-2 text-gray-500 text-[14px]">Preço</label>
         <div className="relative w-full mb-4">
           <select
-        value={faixaPreco}
-        onChange={(e) => setFaixaPreco(e.target.value)}
-        className="p-6 border rounded-md w-full appearance-none"
+            value={faixaPreco}
+            onChange={(e) => setFaixaPreco(e.target.value)}
+            className="p-6 border rounded-md w-full appearance-none"
           >
-        <option value="R$0–R$2,500">R$0–R$2,500</option>
-        <option value="R$2,500–R$5,000">R$2,500–R$5,000</option>
-        <option value="R$5,000+">R$5,000+</option>
+            <option value="R$0–R$2,500">R$0–R$2,500</option>
+            <option value="R$2,500–R$5,000">R$2,500–R$5,000</option>
+            <option value="R$5,000+">R$5,000+</option>
           </select>
           {/* Ícone da seta para baixo no mobile */}
           <DownArrowIcon className="absolute right-4 top-1/2 transform -translate-y-1/2 text-purple-500" />
         </div>
-        <label className="mb-2 text-gray-500 text-[14px]">Tipo de Propriedade</label>
+        <label className="mb-2 text-gray-500 text-[14px]">
+          Tipo de Propriedade
+        </label>
         <div className="relative w-full mb-4">
           <select
-        value={tipoPropriedade}
-        onChange={(e) => setTipoPropriedade(e.target.value)}
-        className="p-6 border rounded-md w-full appearance-none"
+            value={tipoPropriedade}
+            onChange={(e) => setTipoPropriedade(e.target.value)}
+            className="p-6 border rounded-md w-full appearance-none"
           >
-        <option value="">Todas</option>{" "}
-        {/* Opção vazia para remover o filtro */}
-        {categorias.map((categoria) => (
-          <option key={categoria} value={categoria}>
-            {categoria.charAt(0).toUpperCase() + categoria.slice(1)}
-          </option>
-        ))}
+            <option value="">Todas</option>
+            {/* Opção vazia para remover o filtro */}
+            {categorias.map((categoria) => (
+              <option key={categoria} value={categoria}>
+                {categoria.charAt(0).toUpperCase() + categoria.slice(1)}
+              </option>
+            ))}
           </select>
           {/* Ícone da seta para baixo no mobile */}
           <DownArrowIcon className="absolute right-4 top-1/2 transform -translate-y-1/2 text-purple-500" />
