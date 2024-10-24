f5de80647dea01e6aa8dfde572a726fe50bbc9ef
Felipe Lima <pegasusyt1@gmail.com>
2024-10-23 23:27:35 -0300

    ajustes no mapa

--- website/src/components_i/ui/MapComponent.tsx
+++ website/src/components_i/ui/MapComponent.tsx
@@ -20,7 +20,44 @@ const MapComponent: React.FC<MapComponentProps> = ({ latitude, longitude }) => {
     return <p>API Key para Google Maps não encontrada.</p>;
   }
 
-  console.log("Renderizando mapa com lat:", latitude, "e lng:", longitude);
+  const mapOptions = {
+    disableDefaultUI: true, // Remove todos os controles
+    zoomControl: false, // Remove o controle de zoom
+    streetViewControl: false, // Remove o boneco de street view
+    fullscreenControl: false, // Remove o botão de tela cheia
+    styles: [
+      {
+        featureType: "all",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+    ], // Estilos para esconder todos os textos e marcadores
+  };
 
   return (
     <div className="w-full h-full">
@@ -29,6 +66,7 @@ const MapComponent: React.FC<MapComponentProps> = ({ latitude, longitude }) => {
           mapContainerStyle={containerStyle}
           center={{ lat: latitude, lng: longitude }}
           zoom={15}
+          options={mapOptions} // Aplicar as opções customizadas
         >
           <Marker position={{ lat: latitude, lng: longitude }} />
         </GoogleMap>
