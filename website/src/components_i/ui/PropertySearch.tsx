a2b8e7ccd8dad19a3964a3a5cfbbf8fe230d4fec
Felipe Lima <pegasusyt1@gmail.com>
2024-10-25 15:08:53 -0300

    adiciona a opcao qualquer a banheiros e quartos

--- website/src/components_i/ui/PropertySearch.tsx
+++ website/src/components_i/ui/PropertySearch.tsx
@@ -34,8 +34,8 @@ const PropertySearch: React.FC<PropertySearchProps> = ({ onSearch }) => {
   const [precoMaximo, setPrecoMaximo] = useState(5000);
   const [categoria, setCategoria] = useState("");
   const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const [quarto, setQuarto] = useState(1); // Estado para quarto
-  const [banheiro, setBanheiro] = useState(1); // Estado para banheiro
+  const [quarto, setQuarto] = useState<number | "Qualquer">("Qualquer"); // Estado para quarto
+  const [banheiro, setBanheiro] = useState<number | "Qualquer">("Qualquer"); // Estado para banheiro
 
   // Função para enviar os filtros ao componente pai e fechar o modal
   const handleSearch = () => {
@@ -44,8 +44,8 @@ const PropertySearch: React.FC<PropertySearchProps> = ({ onSearch }) => {
       precoMinimo: precoMinimo || 0, // Garante que o valor mínimo seja 0 caso não esteja definido
       precoMaximo: precoMaximo || 50000, // Garante um valor máximo padrão
       categoria,
-      quarto,
-      banheiro,
+      quarto: quarto === "Qualquer" ? undefined : quarto, // Se "Qualquer" estiver selecionado, remove o filtro
+      banheiro: banheiro === "Qualquer" ? undefined : banheiro, // Se "Qualquer" estiver selecionado, remove o filtro
     };
 
     onSearch(appliedFilters);
@@ -56,26 +56,55 @@ const PropertySearch: React.FC<PropertySearchProps> = ({ onSearch }) => {
   const toggleFilters = () => {
     setIsFilterOpen(!isFilterOpen);
   };
+
+  // Função para remover todos os filtros
+  const clearFilters = () => {
+    setLocalizacao("");
+    setPrecoMinimo(0);
+    setPrecoMaximo(50000);
+    setCategoria("");
+    setQuarto("Qualquer");
+    setBanheiro("Qualquer");
+  };
+
   // Função para incrementar o número de quartos
   const incrementQuarto = () => {
-    setQuarto((prevQuarto) => prevQuarto + 1);
+    if (quarto === "Qualquer") {
+      setQuarto(1);
+    } else {
+      setQuarto((prevQuarto) =>
+        typeof prevQuarto === "number" ? prevQuarto + 1 : 1
+      );
+    }
   };
 
   // Função para decrementar o número de quartos
   const decrementQuarto = () => {
-    setQuarto((prevQuarto) => (prevQuarto > 1 ? prevQuarto - 1 : prevQuarto)); // Evitar que o valor seja menor que 1
+    if (quarto === "Qualquer") {
+      setQuarto(1);
+    } else if (typeof quarto === "number" && quarto > 1) {
+      setQuarto(quarto - 1);
+    }
   };
 
   // Função para incrementar o número de banheiros
   const incrementBanheiro = () => {
-    setBanheiro((prevBanheiro) => prevBanheiro + 1);
+    if (banheiro === "Qualquer") {
+      setBanheiro(1);
+    } else {
+      setBanheiro((prevBanheiro) =>
+        typeof prevBanheiro === "number" ? prevBanheiro + 1 : 1
+      );
+    }
   };
 
   // Função para decrementar o número de banheiros
   const decrementBanheiro = () => {
-    setBanheiro((prevBanheiro) =>
-      prevBanheiro > 1 ? prevBanheiro - 1 : prevBanheiro
-    ); // Evitar que o valor seja menor que 1
+    if (banheiro === "Qualquer") {
+      setBanheiro(1);
+    } else if (typeof banheiro === "number" && banheiro > 1) {
+      setBanheiro(banheiro - 1);
+    }
   };
 
   // Lógica para impedir que o preço mínimo seja maior que o máximo
@@ -172,41 +201,55 @@ const PropertySearch: React.FC<PropertySearchProps> = ({ onSearch }) => {
 
       {/* Modal de Filtros */}
       {isFilterOpen && (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-[450px]">
-            <h2 className="text-xl font-semibold mb-4">Filtros</h2>
-
-            {/* Bloco de Data de Entrada */}
-            <div className="mb-4">
-              <label className="font-semibold text-gray-600">
-                Data de Entrada
-              </label>
-              <input type="date" className="w-full p-2 border rounded-md" />
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
+            <div className="bg-white rounded-xl shadow-lg w-[450px] relative">
+            {/* Navbar Filtros */}
+            <div className="bg-white p-4 flex justify-between items-center shadow rounded-t-xl">
+              <button className="text-blue-800 font-bold" onClick={toggleFilters}>
+                ✕
+              </button>
+              <h2 className="text-lg font-bold text-center flex-1 ml-12">
+                Filtros
+              </h2>
+              {/* Espaço reservado para alinhamento */}
+              <div style={{ width: "64px" }}></div>
             </div>
 
-            {/* Bloco de Data de Saída */}
-            <div className="mb-4">
-              <label className="font-semibold text-gray-600">
-                Data de Saída
-              </label>
-              <input type="date" className="w-full p-2 border rounded-md" />
-            </div>
+            <div className="p-6">
+              {/* Bloco de Data de Entrada */}
+              <div className="mb-4">
+                <label className="font-semibold text-gray-600">
+                  Data de Entrada
+                </label>
+                <input type="date" className="w-full p-2 border rounded-md" />
+              </div>
 
-            {/* Slider Preço Mínimo */}
-            <div className="mb-4">
-              <label className="font-semibold text-gray-600">
-                Preço Mínimo: R$ {precoMinimo}
-              </label>
-              <input
-                type="range"
-                min="0"
-                max="50000"
-                value={precoMinimo}
-                onChange={(e) => handleMinPriceChange(Number(e.target.value))}
-                style={{ ...sliderStyle, ...sliderTrackStyle }}
-              />
-              <style>
-                {`
+              {/* Bloco de Data de Saída */}
+              <div className="mb-4">
+                <label className="font-semibold text-gray-600">
+                  Data de Saída
+                </label>
+                <input type="date" className="w-full p-2 border rounded-md" />
+              </div>
+
+              {/* Divisor */}
+              <hr className="my-4" />
+
+              {/* Slider Preço Mínimo */}
+              <div className="mb-4">
+                <label className="font-semibold text-gray-600">
+                  Preço Mínimo: R$ {precoMinimo}
+                </label>
+                <input
+                  type="range"
+                  min="0"
+                  max="50000"
+                  value={precoMinimo}
+                  onChange={(e) => handleMinPriceChange(Number(e.target.value))}
+                  style={{ ...sliderStyle, ...sliderTrackStyle }}
+                />
+                <style>
+                  {`
                   input[type="range"]::-webkit-slider-thumb {
                     ${Object.entries(sliderThumbStyle)
                       .map(([key, value]) => `${key}: ${value};`)
@@ -223,24 +266,24 @@ const PropertySearch: React.FC<PropertySearchProps> = ({ onSearch }) => {
                       .join(" ")}
                   }
                 `}
-              </style>
-            </div>
+                </style>
+              </div>
 
-            {/* Slider Preço Máximo */}
-            <div className="mb-4">
-              <label className="font-semibold text-gray-600">
-                Preço Máximo: R$ {precoMaximo}
-              </label>
-              <input
-                type="range"
-                min="0"
-                max="50000"
-                value={precoMaximo}
-                onChange={(e) => handleMaxPriceChange(Number(e.target.value))}
-                style={{ ...sliderStyle, ...sliderTrackStyle }}
-              />
-              <style>
-                {`
+              {/* Slider Preço Máximo */}
+              <div className="mb-4">
+                <label className="font-semibold text-gray-600">
+                  Preço Máximo: R$ {precoMaximo}
+                </label>
+                <input
+                  type="range"
+                  min="0"
+                  max="50000"
+                  value={precoMaximo}
+                  onChange={(e) => handleMaxPriceChange(Number(e.target.value))}
+                  style={{ ...sliderStyle, ...sliderTrackStyle }}
+                />
+                <style>
+                  {`
                   input[type="range"]::-webkit-slider-thumb {
                     ${Object.entries(sliderThumbStyle)
                       .map(([key, value]) => `${key}: ${value};`)
@@ -257,73 +300,89 @@ const PropertySearch: React.FC<PropertySearchProps> = ({ onSearch }) => {
                       .join(" ")}
                   }
                 `}
-              </style>
-            </div>
-
-            {/* Bloco de Quartos */}
-            <div className="mb-4">
-              <p className="font-semibold text-gray-600 mb-2">Quartos</p>
-              <div className="flex items-center space-x-4">
-                <button
-                  className="border rounded-full px-3 py-1"
-                  onClick={decrementQuarto}
-                >
-                  -
-                </button>
-                <span>{quarto}+</span>
-                <button
-                  className="border rounded-full px-3 py-1"
-                  onClick={incrementQuarto}
-                >
-                  +
-                </button>
+                </style>
               </div>
-            </div>
 
-            {/* Bloco de Banheiros */}
-            <div className="mb-4">
-              <p className="font-semibold text-gray-600 mb-2">Banheiros</p>
-              <div className="flex items-center space-x-4">
-                <button
-                  className="border rounded-full px-3 py-1"
-                  onClick={decrementBanheiro}
-                >
-                  -
-                </button>
-                <span>{banheiro}+</span>
-                <button
-                  className="border rounded-full px-3 py-1"
-                  onClick={incrementBanheiro}
-                >
-                  +
-                </button>
+              {/* Divisor */}
+              <hr className="my-4" />
+
+              {/* Bloco de Quartos */}
+              <div className="mb-4">
+                <p className="font-semibold text-gray-600 mb-2">Quartos</p>
+                <div className="flex items-center space-x-4">
+                  <button
+                    className="border rounded-full px-3 py-1"
+                    onClick={decrementQuarto}
+                  >
+                    -
+                  </button>
+                  <span>
+                    {quarto === "Qualquer" ? "Qualquer" : `${quarto}+`}
+                  </span>
+                  <button
+                    className="border rounded-full px-3 py-1"
+                    onClick={incrementQuarto}
+                  >
+                    +
+                  </button>
+                </div>
               </div>
-            </div>
 
-            {/* Dropdown de Categorias */}
-            <div className="mb-4">
-              <label className="font-semibold text-gray-600">Categoria</label>
-              <select
-                value={categoria}
-                onChange={(e) => setCategoria(e.target.value)}
-                className="w-full p-2 border rounded-md"
-              >
-                <option value="">Todas as categorias</option>
-                {categorias.map((categoria) => (
-                  <option key={categoria} value={categoria}>
-                    {categoria}
-                  </option>
-                ))}
-              </select>
-            </div>
+              {/* Bloco de Banheiros */}
+              <div className="mb-4">
+                <p className="font-semibold text-gray-600 mb-2">Banheiros</p>
+                <div className="flex items-center space-x-4">
+                  <button
+                    className="border rounded-full px-3 py-1"
+                    onClick={decrementBanheiro}
+                  >
+                    -
+                  </button>
+                  <span>
+                    {banheiro === "Qualquer" ? "Qualquer" : `${banheiro}+`}
+                  </span>
+                  <button
+                    className="border rounded-full px-3 py-1"
+                    onClick={incrementBanheiro}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
 
-            <div className="flex justify-center space-x-4 mt-4">
-              <Button variant="default" onClick={toggleFilters}>
-                Fechar
-              </Button>
-              <Button variant="default" onClick={handleSearch}>
-                Aplicar Filtros
-              </Button>
+              {/* Divisor */}
+              <hr className="my-4" />
+
+              {/* Dropdown de Categorias */}
+              <div className="mb-4">
+                <label className="font-semibold text-gray-600">Categoria</label>
+                <select
+                  value={categoria}
+                  onChange={(e) => setCategoria(e.target.value)}
+                  className="w-full p-2 border rounded-md"
+                >
+                  <option value="">Todas as categorias</option>
+                  {categorias.map((categoria) => (
+                    <option key={categoria} value={categoria}>
+                      {categoria}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              {/* Botões de Ação */}
+              <div className="flex justify-between mt-4">
+                <button
+                  onClick={clearFilters}
+                  className="text-gray-600 bg-white border-none"
+                  style={{ cursor: "pointer" }}
+                >
+                  Remover filtros
+                </button>
+                <Button variant="default" onClick={handleSearch}>
+                  Aplicar Filtros
+                </Button>
+              </div>
             </div>
           </div>
         </div>
