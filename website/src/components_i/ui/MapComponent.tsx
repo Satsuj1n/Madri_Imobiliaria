12caf7d6a1147c97352a165b7453d398bdac7657
Felipe Lima <pegasusyt1@gmail.com>
2024-10-28 15:40:10 -0300

    componente do mapa funcionando perfeitamente

--- website/src/components_i/ui/MapComponent.tsx
+++ website/src/components_i/ui/MapComponent.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, OverlayView } from "@react-google-maps/api";
+import { ReactComponent as BedIcon } from "../../assets/icons/bedIcon.svg";
+import { ReactComponent as BathIcon } from "../../assets/icons/bathIcon.svg";
+import { ReactComponent as SizeIcon } from "../../assets/icons/sizeIcon.svg";
 
 // Atualizamos a interface para lidar com imóveis de aluguel e venda
 interface Imovel {
@@ -8,6 +11,10 @@ interface Imovel {
   titulo: string;
   aluguelValor?: number; // Opcional para imóveis de aluguel
   valor?: number; // Opcional para imóveis de venda
+  imagemPrincipal?: string; // URL da imagem principal do imóvel
+  quarto?: number; // Número de quartos
+  banheiro?: number; // Número de banheiros
+  area?: number; // Área do imóvel em m²
 }
 
 interface MapComponentProps {
@@ -16,11 +23,17 @@ interface MapComponentProps {
 
 const MapComponent: React.FC<MapComponentProps> = ({ imoveis }) => {
   const [coordinatesList, setCoordinatesList] = useState<
-    { lat: number; lng: number; _id: string; preco: number }[] // Modificado para lidar com o preço genérico
+    { lat: number; lng: number; _id: string; preco: number }[]
   >([]);
   const [mapBounds, setMapBounds] = useState<google.maps.LatLngBounds | null>(
     null
   );
+  const [selectedImovel, setSelectedImovel] = useState<Imovel | null>(null); // Estado para o imóvel selecionado
+  const [selectedPosition, setSelectedPosition] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null); // Posição do imóvel selecionado
+  const [mapInstance, setMapInstance] = useState<google.maps.Map | null>(null); // Referência ao mapa
 
   const containerStyle = {
     width: "100%",
@@ -109,7 +122,9 @@ const MapComponent: React.FC<MapComponentProps> = ({ imoveis }) => {
       }
 
       setCoordinatesList(newCoordinatesList);
-      setMapBounds(bounds);
+      if (newCoordinatesList.length > 0) {
+        setMapBounds(bounds);
+      }
     };
 
     if (imoveis.length > 0) {
@@ -125,19 +140,37 @@ const MapComponent: React.FC<MapComponentProps> = ({ imoveis }) => {
     return <p>API Key para Google Maps não encontrada.</p>;
   }
 
+  const handleMarkerClick = (imovelId: string, lat: number, lng: number) => {
+    const imovel = imoveis.find((item) => item._id === imovelId);
+    if (imovel && mapInstance) {
+      setSelectedImovel(imovel); // Armazena o imóvel selecionado no estado
+      setSelectedPosition({ lat, lng }); // Define a posição selecionada
+      mapInstance.panTo({ lat, lng }); // Centraliza o mapa no imóvel selecionado
+      mapInstance.setZoom(18); // Ajusta o zoom para um valor mais aproximado
+    }
+  };
+
+  const handleCloseCard = () => {
+    setSelectedImovel(null);
+    setSelectedPosition(null);
+  };
+
   return (
-    <div className="w-full h-full">
+    <div className="relative w-full h-full">
       <LoadScript googleMapsApiKey={apiKey}>
         <GoogleMap
           mapContainerStyle={containerStyle}
-          zoom={coordinatesList.length > 1 ? undefined : 16} // Se houver mais de um imóvel, ajusta automaticamente, senão aplica zoom fixo
+          zoom={selectedPosition ? 18 : 16} // Mantém o zoom padrão se não houver posição selecionada
           center={
-            mapBounds
+            selectedPosition
+              ? selectedPosition
+              : mapBounds
               ? mapBounds.getCenter().toJSON()
               : { lat: -15.7942, lng: -47.8825 } // Brasília como fallback
           }
           options={mapOptions}
           onLoad={(map) => {
+            setMapInstance(map); // Guarda a referência do mapa
             if (mapBounds) {
               map.fitBounds(mapBounds); // Ajusta o zoom e o centro para cobrir todos os imóveis
             }
@@ -149,12 +182,76 @@ const MapComponent: React.FC<MapComponentProps> = ({ imoveis }) => {
               position={{ lat: coords.lat, lng: coords.lng }}
               mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
             >
-              <div className="bg-white text-[#0053f8] p-2 px-6 flex justify-center items-center text-center rounded-lg shadow-lg font-bold text-nowrap">
-                R${coords.preco}{" "}
-                {/* Exibindo o valor genérico (aluguel ou venda) */}
+              <div
+                className="price-box bg-white text-[#0053f8] p-2 flex justify-center items-center text-center rounded-lg shadow-lg font-bold cursor-pointer px-6"
+                style={{ minWidth: `${coords.preco.toString().length * 10}px` }}
+                onClick={() =>
+                  handleMarkerClick(coords._id, coords.lat, coords.lng)
+                } // Ação ao clicar no box
+              >
+                R${coords.preco}
               </div>
             </OverlayView>
           ))}
+
+          {/* Card que mostra o imóvel selecionado */}
+          {selectedImovel && selectedPosition && (
+            <OverlayView
+              position={{
+                lat: selectedPosition.lat,
+                lng: selectedPosition.lng,
+              }}
+              mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+              getPixelPositionOffset={() => ({ x: -150, y: -220 })} // Ajusta o card para aparecer acima do marcador e centralizado
+            >
+              <div className="bg-white rounded-lg shadow-lg p-4 mb-6 w-80 relative">
+                <button
+                  className="absolute font-bold top-0 left-0 text-xl m-2 text-blue-700 hover:text-blue-950"
+                  onClick={handleCloseCard}
+                >
+                  ✕
+                </button>
+                <h2 className="text-lg font-bold mb-2 text-center">
+                  {selectedImovel.titulo}
+                </h2>
+                {selectedImovel.imagemPrincipal ? (
+                  <img
+                    src={selectedImovel.imagemPrincipal}
+                    alt={selectedImovel.titulo}
+                    className="w-full h-48 object-cover rounded-md"
+                  />
+                ) : (
+                  <div className="w-full h-48 bg-gray-200 rounded-md flex items-center justify-center">
+                    <span>Imagem não disponível</span>
+                  </div>
+                )}
+
+                {/* Ícones com informações */}
+                <div className="flex items-center justify-start gap-6 mt-4">
+                  <div className="flex items-center">
+                    <BedIcon />
+                    <span className="text-[#6C727F] ml-2 font-normal text-sm">
+                      {selectedImovel.quarto || "N/A"}
+                    </span>
+                  </div>
+                  <div className="flex items-center">
+                    <BathIcon />
+                    <span className="text-[#6C727F] ml-2 font-normal text-sm">
+                      {selectedImovel.banheiro || "N/A"}
+                    </span>
+                  </div>
+                  <div className="flex items-center">
+                    <SizeIcon />
+                    <span className="text-[#6C727F] ml-2 font-normal text-sm">
+                      {selectedImovel.area
+                        ? `${selectedImovel.area} m²`
+                        : "N/A"}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </OverlayView>
+          )}
         </GoogleMap>
       </LoadScript>
     </div>
