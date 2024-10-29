b5731620d1e0f481afad31127ba79c78e12d2b46
kirz4 <lucasrabello098@gmail.com>
2024-10-29 15:59:32 -0300

    animacao BenefitSection feita

--- website/src/components_i/ui/Beneficit.tsx
+++ website/src/components_i/ui/Beneficit.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Beneficit = () => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  // Captura a posição de scroll em relação ao componente
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start 99%", "end start"], // A animação começa mais cedo
+  });
+
+  // Transforma o scroll progress em movimento no eixo X (da esquerda para o centro)
+  const xTransform = useTransform(scrollYProgress, [0, 0.6], ["-100vw", "0vw"]);
+  const opacityTransform = useTransform(scrollYProgress, [0, 0.6], [0, 1]);
+
+  // Adicionando suavidade à animação com 'spring'
+  const transition = {
+    type: "spring", // Animação de mola
+    stiffness: 50,  // Rigidez da mola (menor valor é mais suave)
+    damping: 15,    // Amortecimento (maior valor para menos oscilação)
+  };
+
   return (
-    <div className="bg-[#F7F7FD] border border-[#E0DEF7] rounded-lg w-[90%] md:w-full lg:w-[90%] xl:w-[1120px] h-auto flex flex-col lg:flex-row my-8 lg:my-24 p-4 lg:p-8 mx-auto">
+    <motion.div
+      ref={targetRef}
+      style={{
+        x: xTransform, // Movimenta o componente de acordo com o scroll
+        opacity: opacityTransform, // Ajusta a opacidade de acordo com o scroll
+      }}
+      transition={transition} // Aplicando a transição
+      className="bg-[#F7F7FD] border border-[#E0DEF7] rounded-lg w-[90%] md:w-full lg:w-[90%] xl:w-[1120px] h-auto flex flex-col lg:flex-row my-8 lg:my-24 p-4 lg:p-8 mx-auto"
+    >
       {/* Left Column with text and icons with padding applied */}
       <div className="w-full lg:w-1/2 flex flex-col justify-center px-4 lg:px-10 py-4 lg:py-8">
         <div className="flex flex-col space-y-4">
@@ -10,8 +38,7 @@ const Beneficit = () => {
             A nova maneira de encontrar sua nova casa
           </h2>
           <p className="text-[#000929] text-base lg:text-lg">
-            Encontre o lugar dos seus sonhos para morar com mais de 10k+
-            propriedades listadas.
+            Encontre o lugar dos seus sonhos para morar com mais de 10k+ propriedades listadas.
           </p>
         </div>
 
@@ -69,7 +96,7 @@ const Beneficit = () => {
           className="w-full h-auto lg:h-full object-cover rounded-r-lg"
         />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
